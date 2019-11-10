d9e4df3a4fbb8d3da54ba363243d0a0c052716a6
Mithun Kamath <callmekatootie@gmail.com>
2019-11-10 11:35:29 +0530

    Use different db for test

--- config/test.js
+++ config/test.js
@@ -5,5 +5,6 @@
 module.exports = {
   CHALLENGE_API_URL: 'https://api.topcoder-dev.com/v4/challenges',
   MEMBER_API_URL: 'https://api.topcoder-dev.com/v3/users',
-  MOCK_API_PORT: 3001
+  MOCK_API_PORT: 3001,
+  MONGODB_URL: process.env.TEST_MONGODB_URL || 'mongodb://localhost:27017/leaderboardDB_test'
 }
